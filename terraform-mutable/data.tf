6bf39451a0fd5f5884d8f7f6726754f5337171a4
siddhu <siddhudangeti@gmail.com>
2022-01-19 18:50:03 +0530

    checking of peer connection

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -1,11 +1,4 @@
-#data "aws_ami" "ami_ec2" {
-#  most_recent      = true
-#  name_regex       = "base-ec2"
-#  owners           = ["self"]
-#}
-
-data "aws_ami" "ec2-ami" {
-  executable_users = ["self"]
+data "aws_ami" "ami_ec2" {
   most_recent      = true
   name_regex       = "base-ec2"
   owners           = ["self"]
--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,19 +1,8 @@
-#resource "aws_spot_instance_request" "sample" {
-#  count         = var.INSTANCES_NO
-#  ami           = data.aws_ami.ami_ec2.id
-#  instance_type = var.INSTANCES_TYPE
-#
-#  tags = {
-#    Name = "${ENV}-${COMPONENT}-${count.index +1 }"
-#  }
-#}
-
-
-
-resource "aws_instance" "example" {
-  ami = data.aws_ami.ec2-ami.id
+resource "aws_spot_instance_request" "ec2-spot" {
+  count         = var.INSTANCES_NO
+  ami           = data.aws_ami.ec2-ami.id
   instance_type = var.INSTANCES_TYPE
-  lifecycle {
-    ignore_changes = [ami]
+  tags = {
+    Name = "${ENV}-${COMPONENT}-${count.index+1}"
   }
 }
