f06d6565d18e657669d5b000013073d9566276e2
siddhu <siddhudangeti@gmail.com>
2022-01-19 18:51:08 +0530

    ec2 creation

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "ec2-spot" {
   count         = var.INSTANCES_NO
-  ami           = data.aws_ami.ec2-ami.id
+  ami           = data.aws_ami.ami_ec2.id
   instance_type = var.INSTANCES_TYPE
   tags = {
     Name = "${ENV}-${COMPONENT}-${count.index+1}"
