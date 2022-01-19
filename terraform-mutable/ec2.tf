62c8bcfc438b0124b28441b64c7ff55165e5b5f2
siddhu <siddhudangeti@gmail.com>
2022-01-19 21:16:20 +0530

    ec2 creation along with tag

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,13 +1,15 @@
 resource "aws_spot_instance_request" "ec2-spot" {
-  count         = var.INSTANCES_NO
-  ami           = data.aws_ami.ami_ec2.id
-  instance_type = var.INSTANCES_TYPE
+  count                = var.INSTANCES_NO
+  ami                  = data.aws_ami.ami_ec2.id
+  instance_type        = var.INSTANCES_TYPE
+  wait_for_fulfillment = "true"
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
   }
 }
+
 resource "aws_ec2_tag" "spot-ec2" {
-  count = length(aws_spot_instance_request.ec2-spot)
+  count       = length(aws_spot_instance_request.ec2-spot)
   resource_id = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   key         = "Name"
   value       = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
