c11c2993092fdf16c5ee8b8eea1451ec15f2baad
siddhu <siddhudangeti@gmail.com>
2022-01-20 23:33:04 +0530

    NEXUS account cridentials are added and locals specified (for command line variables)

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -2,14 +2,22 @@ resource "null_resource" "app-deploy1" {
   count = length(aws_spot_instance_request.ec2-spot)
   connection {
     type     = "ssh"
-    user     = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_USERNAME"]
-    password = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_PASSWD"]
+    user     = local.SSH_USERNAME
+    password = local.SSH_PASSWD
     host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
   }
 
   provisioner "remote-exec" {
     inline = [
-      "ansible-pull -U https://github.com/siddhudeva/ansible-1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION}"
+      "ansible-pull -U https://github.com/siddhudeva/ansible-1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USERNAME = local.NEXUS_USERNAME -e NEXUS_PASSWD = local.NEXUS_PASSWD"
+
     ]
   }
+}
+locals {
+  SSH_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_USERNAME"]
+  SSH_PASSWD = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_PASSWD"]
+  NEXUS_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_USERNAME"]
+  NEXUS_PASSWD = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_PASSWD"]
+
 }
