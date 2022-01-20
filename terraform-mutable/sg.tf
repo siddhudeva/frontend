c226cfba6c42003e3aaa3a16c11272feb6506b5e
siddhu <siddhudangeti@gmail.com>
2022-01-20 11:59:53 +0530

    security group added for ec2 instance

---
+++ terraform-mutable/sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "sg-ec2" {
+  name        = "${var.COMPONENT}}-sg-public"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.COMPONENT}}-sg-public"
+  }
+}
