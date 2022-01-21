3464e41c44b039bfd1242cf1be338d0855077187
siddhu <siddhudangeti@gmail.com>
2022-01-21 11:03:50 +0530

    app-deploy file

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -9,14 +9,14 @@ resource "null_resource" "app-deploy" {
 
   provisioner "remote-exec" {
     inline = [
-      "ansible-pull -U https://github.com/siddhudeva/ansible-1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION}"
+      "ansible-pull -U https://github.com/siddhudeva/ansible-1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION} -e NEXUS_USERNAME=${local.NEXUS_USERNAME}" -e NEXUS_PASSWORD=${local.NEXUS_PASSWD}
     ]
   }
 }
 locals {
   SSH_USERNAME = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_USERNAME"])
   SSH_PASSWD = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["SSH_PASSWD"])
-#  NEXUS_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_USERNAME"]
-#  NEXUS_PASSWD = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_PASSWD"]
+  NEXUS_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_USERNAME"]
+  NEXUS_PASSWD = jsondecode(data.aws_secretsmanager_secret_version.secret-ssh.secret_string)["NEXUS_PASSWD"]
 
 }
