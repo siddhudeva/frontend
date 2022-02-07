aa629c05a1d5bffff05df6f7994e0d912ae08931
siddhu <siddhudangeti@gmail.com>
2022-02-07 10:05:34 +0530

    all components- revision -01 (terraform)

--- terraform-mutable/provider.tf
+++ terraform-mutable/provider.tf
@@ -1,8 +1,7 @@
-//// providers are nothing but sources.(where we play)
 provider "aws" {
   region = "us-east-1"
 }
-//// S3 also comes under provider because it is providing statefiles
+
 terraform {
   backend "s3" {}
 }
--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -1,6 +1,8 @@
-/// variables are used to specify the variables
 variable "INSTANCES_NO" {}
 variable "INSTANCES_TYPE" {}
 variable "COMPONENT" {}
 variable "ENV" {}
 variable "APP_VERSION" {}
+#variable "APP_PORT" {}
+#variable "LB_RULE_PRIORITY" {}
+#variable "LB_PRIVATE" {}
